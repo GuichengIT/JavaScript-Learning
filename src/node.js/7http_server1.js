07814c3bce63ef8c9652fa411f192109fb435778
zgc <zhangguicheng_1995@163.com>
2019-09-04 09:09:28 +0800

    update

---
+++ src/node.js/7http_server1.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+
+//1. 创建server
+var server = http.createServer();
+
+//2. 设置服务类型和相应的服务处理函数
+server.on("request", function (req, res) {
+    var url = req.url;
+    res.setHeader("content-type", "text/html; charset=utf-8" );
+    res.end("<p>你好，<a href='#'>世界</a></p>");
+});
+
+//3. 开始监听
+server.listen(3000,function () {
+   console.log("服务开启了...");
+});
