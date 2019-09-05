3413024bf346a0b09c90e50ffef36a381a59498f
zgc <zhangguicheng_1995@163.com>
2019-09-05 08:46:34 +0800

    update

--- src/node.js/7http_server1.js
+++ src/node.js/7http_server1.js
@@ -7,8 +7,13 @@ var server = http.createServer();
 //2. 设置服务类型和相应的服务处理函数
 server.on("request", function (req, res) {
     var url = req.url;
-    res.setHeader("content-type", "text/html; charset=utf-8" );
-    res.end("<p>你好，<a href='#'>世界</a></p>");
+    if(url === "/"){
+        res.setHeader("content-type", "text/html; charset=utf-8" );
+        res.end("<p>你好，<a href='#'>世界</a></p>");
+    }else{
+        // res.setHeader("content-type", "text/plain; charset=utf-8" );
+        res.end("出错了");
+    }
 });
 
 //3. 开始监听
