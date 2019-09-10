95fe225546b3436ffacea2e5d50cd33a67c21427
zgc <zhangguicheng_1995@163.com>
2019-09-10 21:42:32 +0800

    update

--- src/node.js/1http.js
+++
@@ -1,9 +0,0 @@
-// 接下来要要做一件使用node比较有成就感的一件事
-// 使用node构建一台服务器
-// 在node中专门提供了一个核心模块
-// http这个模块的职责就是帮你创建编写服务器的
-
-//1. 加载http核心模块
-var http = require("http");
-//2. 使用http.carateServer()方法创建一个Web服务器
-// 返回一个server实例
