67cd4616bdc7d676bc03cfe74e9e04d50c778dfa
zgc <zhangguicheng_1995@163.com>
2019-09-16 11:49:02 +0800

    update

--- src/Ajax/nodeServer.js
+++ src/Ajax/nodeServer.js
@@ -8,24 +8,8 @@ var server = http.createServer();
 // 设置服务类型，服务请求函数
 server.on('request', function (req, res) {
     var url = req.url;
-    var wwwDir = 'E:\\FrontEndProject\\JavaScript-Learning\\src\\jQuery';
-    var file_index = '/index.html'; //用户端口号后不输入时默认响应index.html文件
-    if (url === '/'){
-        url = file_index;
-    }
-    fs.readFile(wwwDir+url, function (err, data) {
-        console.log(wwwDir+url);
-        if (err){
-            res.setHeader('content-type', 'text/plain;chaset=utf-8')
-            res.end('404 Not Found');
-            console.log('出错啦');
-        } else {
-            res.setHeader('content-type', 'text/html;chaset=utf-8')
-            res.end(data);
-            res.end('hello ajax');
-            console.log('读取成功');
-        }
-    });
+    console.log(req.url);
+   
 });
 
 //绑定端口，开启服务
